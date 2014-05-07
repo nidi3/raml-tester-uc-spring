fcebfbf6b7a502b466db2027c1a1abb1aa372b83
Stefan Niederhauser <stefan.niederhauser@mimacom.com>
2014-05-07 02:55:27 +0200

    update dependency

--- src/test/java/guru/nidi/ramltester/uc/spring/SimpleTest.java
+++ src/test/java/guru/nidi/ramltester/uc/spring/SimpleTest.java
@@ -1,11 +1,10 @@
 package guru.nidi.ramltester.uc.spring;
 
-import guru.nidi.ramltester.RamlLoaders;
-import guru.nidi.ramltester.spring.RamlMatchers;
+import guru.nidi.ramltester.RamlDefinition;
+import guru.nidi.ramltester.spring.RequestResponseMatchers;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.raml.model.Raml;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ContextConfiguration;
@@ -15,6 +14,7 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
+import static guru.nidi.ramltester.spring.RamlResultMatchers.requestResponse;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -30,18 +30,20 @@ public class SimpleTest {
     private WebApplicationContext wac;
 
     private MockMvc mockMvc;
-    private Raml api;
+    private RamlDefinition api;
+    private RequestResponseMatchers requestResponse;
 
     @Before
     public void setup() {
         mockMvc = MockMvcBuilders.webAppContextSetup(wac).build();
-        api = RamlLoaders.fromClasspath(getClass(), "api.yaml");
+        api = RamlDefinition.fromClasspath(getClass(), "api.yaml");
+        requestResponse = requestResponse().withServletUri("http://nidi.guru/raml/simple/v1");
     }
 
     @Test
     public void greeting() throws Exception {
         this.mockMvc.perform(get("/greeting").accept(MediaType.parseMediaType("application/json")))
-                .andExpect(RamlMatchers.matchesRaml(api))
+                .andExpect(requestResponse.matchesRaml(api))
                 .andExpect(status().isOk())
                 .andExpect(content().contentTypeCompatibleWith("application/json"))
                 .andExpect(jsonPath("$.content").value("Hello, World!"));
